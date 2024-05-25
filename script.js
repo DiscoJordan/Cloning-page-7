9b62e09379380f42e6773c8263b9710cb2ee16df
Gleb Muravitsky <56343088+DiscoJordan@users.noreply.github.com>
2024-05-25 23:24:07 +0200

    added animatins, responsive design

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict"
+$(document).ready(function (){
+    $('.burger').click(function(event) {
+        $('.side-bar, .content').toggleClass('active');
+    });
+    $('.close').click(function(event) {
+        $('.terms').toggleClass('active');
+    });
+    $('.hide').click(function(event) {
+        $('.header__reminder,main').toggleClass('active');
+    });
+
+    $('.banner__close').click(function(event) {
+        $('.banner').toggleClass('active');
+    });
+
+
+    $('#first-button').click(function(event) {
+        $('#first-button,#first-subscribed,#first-subscribe').toggleClass('active');
+    });
+    $('#second-button').click(function(event) {
+        $('#second-button,#second-subscribed,#second-subscribe').toggleClass('active');
+    });
+    $('#third-button').click(function(event) {
+        $('#third-button,#third-subscribed,#third-subscribe').toggleClass('active');
+    });
+    
+});
